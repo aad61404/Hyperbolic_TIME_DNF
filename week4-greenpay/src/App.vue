fb8595dc81ef92439372e22931a67cc3b53d2a89
aad61404 <asd61404@gmail.com>
2019-12-18 14:26:38 +0800

    green-pay

---
+++ week4-greenpay/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import url('../public/stylesheets/style.min.css');
+*, *::before, *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+</style>
---
+++ week4-greenpay/src/router.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
+    {
+      path: '/',
+      name: 'greenpay',
+      component: () => import('./views/Greenpay.vue'),
+      children: [
+        {
+          // 当 /user/:id/profile 匹配成功，
+          // UserProfile 会被渲染在 User 的 <router-view> 中
+          path: 'credit',
+          component: () => import('./components/Credit.vue')
+        },
+        {
+          path: 'web_atm',
+          component: () => import('./components/Web_ATM.vue')
+        },
+        {
+          path: 'machine_atm',
+          component: () => import('./components/Machine_ATM.vue')
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/finished',
+      name: 'finished',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('./views/Finished.vue')
+    }
+  ]
+})
